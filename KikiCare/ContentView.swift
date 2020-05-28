0b8c651fa9803fbb521332bf6ed452b8ae1ce0a9
Abdelaziz Mezri <abdelaziz.mezriesprit.tn>
2020-05-28 08:05:24 -0700

    Initial Commit

---
+++ KikiCare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KikiCare
+//
+//  Created by Mezri Abdealziz on 5/28/20.
+//  Copyright © 2020 YAApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
